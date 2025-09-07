4feb77711df19f52746c2fdd53bb912282d56d43
Umair Imran <umairva7@gmail.com>
2025-09-08 00:12:41 +0500

    Simple Routing

--- node-crash-course/server.js
+++ node-crash-course/server.js
@@ -2,14 +2,34 @@ import http from 'http';
 
 const server=http.createServer((req,res)=>{
     //res.write('Hello World from server.js');
-    res.setHeader('Content-Type','application/json');
-    res.statusCode=200;
+    if(req.url==='/'){
+         res.setHeader('Content-Type','text/html');
+        res.write('Homepage');
+        res.end();
+    }
+    else if(req.url==='/api/users'){
+         res.setHeader('Content-Type','text/html');
+        res.write('Hello World from API');
+        res.end();
+    }
+    else{
+         res.setHeader('Content-Type','text/html');
+        res.write('Not Found');
+        res.end();
+    }
+    
+
+    //request object
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.headers);
+
     const user={
         name:'Umair',
         age:30
     }
-    res.write(JSON.stringify(user));
-    res.end();
+    // res.write(JSON.stringify(user));
+    // res.end();
 
 });
 const PORT=5000 || process.env.PORT;
