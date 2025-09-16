4516c6a06cce22260789f498e967bf882357625e
Umair Imran <umairva7@gmail.com>
2025-09-16 12:34:59 +0500

    Static WebServer

--- express-crash-course/server.js
+++ express-crash-course/server.js
@@ -3,16 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile
-  (path.join(__dirname, 'public', 'index.html'));
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-});
-app.get('/about', (req, res) => {
-  res.sendFile
-  (path.join(__dirname, 'public', 'about.html'));
 
-});
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
 
