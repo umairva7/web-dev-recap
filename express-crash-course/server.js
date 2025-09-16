8f6c43cd303ae90e0f09f70ed3e50bb27df7dfe9
Umair Imran <umairva7@gmail.com>
2025-09-16 12:29:44 +0500

    res.sendFile() Method

--- express-crash-course/server.js
+++ express-crash-course/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile
+  (path.join(__dirname, 'public', 'index.html'));
+
+});
+app.get('/about', (req, res) => {
+  res.sendFile
+  (path.join(__dirname, 'public', 'about.html'));
+
 });
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
